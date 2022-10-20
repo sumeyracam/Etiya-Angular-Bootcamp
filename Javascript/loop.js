d31b7537f22440158c7481f8698d936d0c980eed
sumeyracam <50549874+sumeyracam@users.noreply.github.com>
2022-10-20 14:23:43 +0300

    Add files via upload

---
+++ Javascript/loop.js
@@ -0,0 +1,11 @@
+console.log("loop startt")
+
+
+let urunler = [
+    {fiyat : 100, ad : "urun1"},
+    {fiyat : 150, ad : "urun2"},
+    {fiyat : 200, ad : "urun3"},
+]
+array.forEach(urunler => {
+    fiyat *= 0.10
+});
