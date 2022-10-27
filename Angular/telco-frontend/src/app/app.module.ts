0d488656aeebfcee0b7b060a3052e86ceb765dda
Sumeyra Cam <sumeyra.cam@yandex.com>
2022-10-27 20:54:34 +0300

    ..

--- Angular/telco-frontend/src/app/app.module.ts
+++ Angular/telco-frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './components/calculator/calculator.component';
 import { ListviewComponent } from './components/listview/listview.component';
+import { ProductlistComponent } from './components/productlist/productlist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculatorComponent,
-    ListviewComponent
+    ListviewComponent,
+    ProductlistComponent
   ],
   imports: [
     BrowserModule,
