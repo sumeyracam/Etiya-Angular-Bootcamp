aa6e6f580e6451fbf905b4a5c1ba88d6eb4ae581
Sumeyra Cam <sumeyra.cam@yandex.com>
2022-11-01 21:01:48 +0300

    pipe and interceptor

--- Angular/telco-frontend/src/app/app.module.ts
+++ Angular/telco-frontend/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { CalculatorComponent } from './components/calculator/calculator.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ListviewComponent } from './components/listview/listview.component';
 import { NgModule } from '@angular/core';
+import { LoadingInterceptor } from './interceptors/loading.interceptor';
+import { CreateFakeArrayPipe } from './pipes/create-fake-array.pipe';
+import { SplitPipe } from './pipes/split.pipe';
 
 @NgModule({
-  declarations: [AppComponent, CalculatorComponent, ListviewComponent],
+  declarations: [AppComponent, CalculatorComponent, ListviewComponent, CreateFakeArrayPipe, SplitPipe],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -17,8 +21,9 @@ import { NgModule } from '@angular/core';
     ReactiveFormsModule,
   ],
   exports: [],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
-
 export class AppModule {}
